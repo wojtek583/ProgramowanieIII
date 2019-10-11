f4684ca0d653cd7d3915f086c9ac0b2e33c392f9
wojtek583 <wojtek583@op.pl>
2019-10-11 08:29:10 +0200

    1Laborki

---
+++ Laborki1Programowanie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laborki1Programowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
