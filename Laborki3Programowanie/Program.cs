9347d5aed3c097ba093d3d42b32f55a6028918ac
wojtek583 <wojtek583@op.pl>
2019-10-18 10:17:18 +0200

    Nedokonczone zadanie ze srednimi.

--- Laborki3Programowanie/Program.cs
+++ Laborki3Programowanie/Program.cs
@@ -1,32 +1,76 @@
 ﻿using System;
 
+/*
+        klasy, nauczyc sie
+
+        petla foreach //w kazdej iteracji zwraca 1 element
+
+        
+
+
+
+*/
 namespace Laborki3Programowanie
 {
     class Program
     {
-        static void Main(string[] args)
+        public class Przedmiot
         {
-            int iloscOcen = -1;
-            int suma = 0;
-            string tekst = string.Empty;
-            while (tekst != "0")
+            string nazwaPrzedmiotu = string.Empty;
+            public void ustawNazwe(string n1)
             {
-                tekst = Console.ReadLine();
-                int ocena;
-                bool czyPrzekonwertowano = int.TryParse(tekst, out ocena);
-                if (czyPrzekonwertowano)
-                {
-                    // TODO
-                    suma += ocena;
-                    iloscOcen++;
-                }
-                else
-                {
-                    Console.WriteLine("Podano niepoprawno liczpe");
-                }
+                nazwaPrzedmiotu = n1;
             }
-            float srednia = (float)suma / iloscOcen;
-            Console.WriteLine(srednia);
+            static float Srednia()
+            {
+                int iloscOcen = 0;
+                float suma = 0;
+                string tekst = string.Empty;
+                while (tekst != "0")
+                {
+                    tekst = Console.ReadLine();
+                    float ocena;
+                    bool czyPrzekonwertowano = float.TryParse(tekst, out ocena);
+                    if (czyPrzekonwertowano)
+                    {
+                        if (ocena > 0 && ocena <= 6)
+                        {
+                            suma += ocena;
+                            iloscOcen++;
+                        }
+                        else if (ocena == 0)
+                        {
+                            Console.WriteLine("Wynik: ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Podano niepoprawno liczbe");
+                        }
+                        // TODO
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Podano niepoprawna liczbe");
+                    }
+
+                }
+                float srednia = suma / iloscOcen;
+                return srednia;
+            }
+            
+        }
+        static void Main(string[] ar4gs)
+        {
+            Przedmiot przedmiot1 = new Przedmiot();
+            Przedmiot przedmiot2 = new Przedmiot();
+            Przedmiot przedmiot3 = new Przedmiot();
+            string txt = Console.ReadLine();
+            przedmiot2.ustawNazwe(txt);
+            
+            Console.WriteLine(przedmiot1.nazwaPrzedmiotu);
+            
+
         }
     }
 }
