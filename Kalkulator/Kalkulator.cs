695f63a889228c42eaf1c86ac689fa09f5f48577
wojtek583 <wojtek583@op.pl>
2019-10-25 10:35:12 +0200

    Kalkulator, 4 zajecia Programowanie

---
+++ Kalkulator/Kalkulator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kalkulator
+{
+    class Kalkulator
+    {
+        public static double dodaj(int x, int y)
+        {
+            return x + y;
+        }
+        public static double dodaj(string x, string y)
+        {
+            int liczba1 = Convert.ToInt32(x);
+            int liczba2 = Convert.ToInt32(y);
+            return dodaj(liczba1, liczba2);
+        }
+        public static double odejmij(int x, int y)
+        {
+            return x - y;
+        }
+        public static double mnoz(int x, int y)
+        {
+            return x * y;
+        }
+        public static double dziel(int x, int y, out bool powodzenie)
+        {
+
+            if (y == 0)
+            {
+                powodzenie = false;
+                return 0;
+            }
+            powodzenie = true;
+            return (double)x / y;
+
+        }
+        public static double PodzielZWyjatkiem(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (double)x / y;
+        }
+
+        public static string wczytajLiczbe(string licznik)
+        {
+            Console.WriteLine("Podaj Liczbe " + licznik + ":");
+            string tekst = Console.ReadLine();
+            return tekst;
+            //return Console.readline()
+        }
+
+    }
+}
---
+++ Kalkulator/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Kalkulator
+{
+    class Komputer
+    {
+
+        public void Opisz()
+        {
+            Console.WriteLine(predkosc + " Ghz " + ram + " GB ");
+        }
+        public double predkosc;
+        public int ram;
+        public int dysk;
+
+    }
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            Komputer komputer1 = new Komputer();
+            komputer1.predkosc = 3.5;
+            komputer1.ram = 16;
+            komputer1.dysk = 3500;
+
+
+            Komputer komputer2 = new Komputer();
+            komputer2.predkosc = 4.5;
+            komputer2.ram = 15;
+            komputer2.dysk = 300;
+
+
+            komputer1.Opisz();
+            komputer2.Opisz();
+
+            string tekst1 = Kalkulator.wczytajLiczbe("1");
+            string tekst2 = Kalkulator.wczytajLiczbe("2");
+
+            Console.WriteLine("Wybierz operacje:  +  -  *  / ");
+            string operacja = Console.ReadLine();
+
+            int liczba1 = Convert.ToInt32(tekst1);
+            int liczba2 = Convert.ToInt32(tekst2);
+
+            switch (operacja)
+            {
+                case "+":
+                    Kalkulator.dodaj(liczba1, liczba2);
+
+
+                    break;
+
+                case "-":
+                    Kalkulator.odejmij(liczba1, liczba2);
+                    break;
+
+                case "*":
+                    Console.WriteLine(Kalkulator.mnoz(liczba1, liczba2));
+                    break;
+
+                case "/":
+                 
+                    try
+                    {
+                        Console.WriteLine(Kalkulator.PodzielZWyjatkiem(liczba1, liczba2));
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Nie dziel przez 0");
+
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Wybrano zly typ operacji");
+
+                    break;
+            }
+        }
+    }
+}
