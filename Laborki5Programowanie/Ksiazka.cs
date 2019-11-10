017d493b3eb3a7798fd07bf403a03aca74bca133
wojtek583 <wojtek583@op.pl>
2019-11-10 22:35:41 +0100

    Poprawienie petli for

---
+++ Laborki5Programowanie/Ksiazka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laborki5Programowanie
+{
+    class Ksiazka
+    {
+        public string Tytul;
+        public string Autor;
+        public int Regal;
+        public int Polka;
+        public int Miejsce;
+
+        public Ksiazka(string tytul, string autor, int R, int P, int M)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            Regal = R;
+            Polka = P;
+            Miejsce = M;
+        }
+        public Ksiazka()
+        {
+
+        }
+    }
+}
