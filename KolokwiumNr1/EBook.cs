ff31b52a171fa91b4b51a765ff6f5073f1c3c961
Wotek583 <wojtek583@op.pl>
2020-01-10 10:41:52 +0100

    doPoprawy nadal

--- KolokwiumNr1/EBook.cs
+++ KolokwiumNr1/EBook.cs
@@ -10,8 +10,8 @@ namespace KolokwiumNr1
         public String Tytul { get; private set; }
         public DateTime DataWydania { get; private set; }
         public DateTime DataOstatniegoZakupu { get; private set; }
-        public double CenaStandardowa { get; set; }
-        public double Obnizka { get; set; }
+        public double CenaStandardowa { get; private set; }
+        public double Obnizka { get; private set; }
         public double AktualnaCena { get; set; }
 
         public EBook(string autor, string tytul, DateTime dataWydania, DateTime dataOstatniegoZakupu, double cenaStandardowa, double obnizka)
@@ -23,7 +23,7 @@ namespace KolokwiumNr1
             if (CenaStandardowa < 0) Console.WriteLine("Podano złą cenę");
             else CenaStandardowa = cenaStandardowa;
             
-            AktualnaCena=CenaStandardowa*((100-obnizka)/100);
+            AktualnaCena=CenaStandardowa*((100-Obnizka)/100);
         }
 
         public void NowaDataZakupu(DateTime nowaDataZakupu)
