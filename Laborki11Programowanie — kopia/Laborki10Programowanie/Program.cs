746fc77c557d5e80a8fe61536fb01e8b5bed7295
Wotek583 <wojtek583@op.pl>
2020-01-17 14:32:14 +0100

    Kol1PoprawaTest

---
+++ Laborki11Programowanie — kopia/Laborki10Programowanie/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Laborki10Programowanie
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
