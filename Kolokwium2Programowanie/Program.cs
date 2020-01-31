b1bb59e1a7f6c5f68b59e8c7ef338ff54c83d57a
Wotek583 <wojtek583@op.pl>
2020-01-31 08:32:46 +0100

    Kolokwium nr2

---
+++ Kolokwium2Programowanie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kolokwium2Programowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
